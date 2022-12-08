7624a91327df06c4cf0523a831730170c037e9df
ameliareiss <98429631+ameliareiss@users.noreply.github.com>
2022-12-08 10:08:53 -0500

    added arg parser

--- SpeedDating.py
+++ SpeedDating.py
@@ -3,14 +3,23 @@ Topic: Speed Dating!
 Algorithms: Bayes Classifier and Decision Trees'''
 
 import numpy as np
+import random
+import argparse
 import pdb
 
-def main():
+parser = argparse.ArgumentParser(description="Train a perceptron to classify letters.")
+parser.add_argument('-s', '--seed', help='seed to control randomness', type=int)
+
+def main(args):
+
     # import data from csv file
+    random.seed(args.seed) # use seed parameter if given
     data = np.genfromtxt("SDdata.csv", dtype=str, delimiter=",", filling_values="x")
-    labels = data[0]
-    print(labels)
+
+    # randomize and separate data into training and testing
+    labels = data[0] # array of column headers
+    data = np.delete(data, 0) # delete the first line of data array before randomizing
 
 
 if __name__ == "__main__":
-    main()
+    main(parser.parse_args())
